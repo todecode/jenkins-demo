0c4c0a384126eb22298ac222f63896ea93994423
todecode <15016414752@163.com>
2019-04-11 10:27:49 +0800

    初始化项目配置

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName HelloController
+ * @Description TODO
+ * @Author Mr.Chen
+ * @Date 上午 10:05
+ * @Version 1.0
+ **/
+@RestController
+public class HelloController {
+
+    @GetMapping("/hi")
+    public String hi(){
+        return "welcome to jenkins ";
+    }
+}
